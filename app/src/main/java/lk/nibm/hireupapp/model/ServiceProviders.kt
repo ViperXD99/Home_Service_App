bd6275be2ef468c18723ae2deedc94f7f9a052d8
ViperXD99 <sandaruxd99@gmail.com>
2023-07-22 23:26:08 +0530

    Service Provider Page
    a separate screen designed to view service providers according to the selected service. all data fetched from firebase and completed with back end

---
+++ app/src/main/java/lk/nibm/hireupapp/model/ServiceProviders.kt
@@ -0,0 +1,17 @@
+package lk.nibm.hireupapp.model
+
+data class ServiceProviders(
+    val NIC_back: String? = null,
+    val NIC_front: String? = null,
+    val address : String? = null,
+    val city: String? = null,
+    val contact: String? = null,
+    val district: String? = null,
+    val dob : String? = null,
+    val full_name: String? = null,
+    val is_verified: String? = null,
+    val nic: String? = null,
+    val photoURL : String? = null,
+    val price: String? = null,
+    val serviceId: String? = null,
+)
