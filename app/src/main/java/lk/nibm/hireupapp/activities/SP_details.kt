30a9d3892825669fe91bc64c177a4ce9ef252aa6
FSHAZNA <126662009+fshazn@users.noreply.github.com>
2023-07-23 18:40:39 +0530

    SP_details Backend, fetching data from firebase

--- app/src/main/java/lk/nibm/hireupapp/activities/SP_details.kt
+++ app/src/main/java/lk/nibm/hireupapp/activities/SP_details.kt
@@ -4,13 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
+import com.bumptech.glide.Glide
 import lk.nibm.hireupapp.R
+import lk.nibm.hireupapp.common.CategoryDataManager
 import lk.nibm.hireupapp.common.ServiceProviderDataManager
 
 class SP_details : AppCompatActivity() {
     private lateinit var spName : TextView
+    private lateinit var spRole : TextView
+    private lateinit var spAddress : TextView
+    private lateinit var spCity : TextView
+    private lateinit var spDistrict : TextView
+    private lateinit var spPrice : TextView
+    private lateinit var spAbout : TextView
+    private lateinit var spProPic : ImageView
+    private lateinit var spStaringDate : TextView
+    private lateinit var spEndingDate : TextView
+    private lateinit var spStartingTime : TextView
+    private lateinit var spEndingTime : TextView
     private lateinit var spBook : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +33,24 @@ class SP_details : AppCompatActivity() {
         val providerData = ServiceProviderDataManager.getProvider()
         initializeComponents()
         clickListeners()
+
         spName.text = providerData?.full_name
-        Toast.makeText(this, providerData?.about, Toast.LENGTH_LONG).show()
+        spRole.text = CategoryDataManager.name
+        spAddress.text = providerData?.address
+        spCity.text = providerData?.city
+        spDistrict.text = providerData?.district
+        spPrice.text= providerData?.price
+        spAbout.text = providerData?.about
+        spStaringDate.text = providerData?.startingDate
+        spEndingDate.text =providerData?.endDate
+        spStartingTime.text = providerData?.startingTime
+        spEndingTime.text = providerData?.endTime
+        Glide.with(this)
+            .load(providerData?.photoURL)
+            .into(spProPic)
+        /*spWorkingHours.text = CategoryDataManager.*/
+
+
     }
 
     private fun clickListeners() {
@@ -31,6 +62,18 @@ class SP_details : AppCompatActivity() {
 
     private fun initializeComponents() {
         spName = findViewById(R.id.sp_name)
+        spRole = findViewById(R.id.sp_role)
+        spAddress = findViewById(R.id.sp_address)
+        spCity = findViewById(R.id.sp_city)
+        spDistrict = findViewById(R.id.sp_district)
+        spPrice = findViewById(R.id.sp_price)
+        spAbout = findViewById(R.id.sp_about)
+        spStaringDate = findViewById(R.id.sp_startingDate)
+        spEndingDate = findViewById(R.id.sp_endingDate)
+        spStartingTime =findViewById(R.id.sp_startingTime)
+        spEndingTime = findViewById(R.id.sp_endingTime)
         spBook = findViewById(R.id.sp_book)
+        spProPic = findViewById(R.id.sp_profile_pic)
+
     }
 }
