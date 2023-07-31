9fbab5ce9c2617819229d1ee28c32afe109df115
ViperXD99 <sandaruxd99@gmail.com>
2023-07-31 10:50:27 +0530

    Top rated and category click events

--- app/src/main/java/lk/nibm/hireupapp/adapter/CategoryAdapter.kt
+++ app/src/main/java/lk/nibm/hireupapp/adapter/CategoryAdapter.kt
@@ -1,13 +1,17 @@
 package lk.nibm.hireupapp.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import lk.nibm.hireupapp.R
+import lk.nibm.hireupapp.activities.ServiceProviders
 import lk.nibm.hireupapp.model.Category
 
 class CategoryAdapter(private val itemList: List<Category>) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
@@ -21,6 +25,13 @@ class CategoryAdapter(private val itemList: List<Category>) : RecyclerView.Adapt
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = itemList[position]
+        holder.cardView.setOnClickListener {
+//            Toast.makeText(getActivity, categoryList[position].name, Toast.LENGTH_SHORT).show()
+            val intent = Intent(holder.itemView.context, ServiceProviders::class.java)
+            intent.putExtra("CATEGORY_NAME", itemList[position].name)
+            intent.putExtra("CATEGORY_ID", itemList[position].id)
+            holder.itemView.context.startActivity(intent)
+        }
         holder.bind(item)
     }
 
@@ -30,13 +41,14 @@ class CategoryAdapter(private val itemList: List<Category>) : RecyclerView.Adapt
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var image: ImageView
         var name: TextView
+        val cardView : LinearLayout
 
 
 
         init {
             image = itemView.findViewById(R.id.imgCategory)
             name = itemView.findViewById(R.id.txtCategoryName)
-
+            cardView = itemView.findViewById(R.id.categoryLayout)
         }
 
         fun bind(item: Category) {
